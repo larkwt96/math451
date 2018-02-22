95c330c6bc44cefa5a25aaa71face9b024e07066
Lucas Wilson <lkwilson96@gmail.com>
2018-02-22 13:23:00 -0700

    problem 1 done

--- h2/p1.py
+++ h2/p1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import math
 
 def setA(n):
     """Post: An is set and global. n is set and global."""
@@ -14,16 +15,37 @@ def getAij(r, c, n):
 
 def delta(r,c):
     """Iij"""
-    return 1 if i == j else 0
+    return 1 if c == r else 0
 
-def setNextA():
-    # iterate for every column, getP, get W, calculate next A
-    # use np.matmul
-    pass # TODO
+def doHouseholdersTransform():
+    global A
+    for c in range(n-2):
+        col = A[:,c][c:]
+        #print('col')
+        #print(col)
+        w = getW(col)
+        #print('w')
+        #print(w)
+        P = getP(w)
+        #print('P')
+        #print(P)
+        A = P*A*P
+        #print('A')
+        #print(A)
+    # zero ends
+    for r in range(n):
+        for c in range(n):
+            if abs(A[r,c]) < 1e-10:
+                A[r,c] = 0
 
 def getP(w):
     """I - 2 wwT"""
-    return [[delta(r,c)-2*w[r]*w[c] for c in range(1,n+1)] for r in range(1,n+1)]
+    P = np.identity(n)
+    base = n-len(w)
+    for c in range(len(w)):
+        for r in range(len(w)):
+            P[base+r,base+c] = P[base+r,base+c] - 2*w[r]*w[c]
+    return P
 
 def getW(Acol):
     """
@@ -31,11 +53,12 @@ def getW(Acol):
     """
     w = np.zeros(len(Acol))
     alpha = getAlpha(Acol)
-    r = getR(alpha)
+    r = getR(Acol, alpha)
     w[0] = 0
     w[1] = (Acol[1]-alpha)/2/r
     for i in range(2, len(w)):
         w[i] = Acol[i]/2/r
+    return w
 
 def getAlpha(Acol):
     """
@@ -44,17 +67,64 @@ def getAlpha(Acol):
     Returns:
         alpha = -sign(A_21)*(sum j=2 to n [a_j1^2])^.5
     """
-    pass # TODO
+    # note A_21 is A[1] since 2 -> 1 and 1 is taken care of
+    n = len(Acol)
+    sign = -1 if Acol[1] < 0 else 1
+    l2Norm = math.sqrt(sum([Acol[j]**2 for j in range(1, n)]))
+    alpha = -sign*l2Norm
+    return alpha
 
-def getR(a):
+def getR(Acol, a):
     """
+    r = (.5*a^2 - .5*A_21*a)^.5
+
     Args:
+        Acol: The working column of A
         a: alpha
     """
-    pass # TODO
+    return math.sqrt(.5*a**2 - .5*Acol[1]*a)
+
+def isSymmetric():
+    points = []
+    for r in range(n):
+        for c in range(r+1, n):
+            if abs(A[r,c]-A[c,r]) > 1e-10:
+                points.append((r,c))
+    return points
+
+def isTridiagonal():
+    points = []
+    for r in range(n):
+        for c in range(r+2, n):
+            if A[r,c] != 0 or A[c,r] != 0:
+                points.append((r,c))
+    return points
+
+def doTest(n):
+    setA(n)
+    print('Householders for n={}'.format(n))
+    print('Before householders transform')
+    print(A)
+    doHouseholdersTransform()
+    print('After householders transform')
+    print(A)
+
+    sym = isSymmetric()
+    if len(sym) > 0:
+        print("Here are points lacking symmetry:")
+        for p in sym:
+            print('({},{})'.format(p[0],p[1]))
+    else:
+        print("Matrix is symmetric")
+
+    tri = isTridiagonal()
+    if len(tri) > 0:
+        print("Here are points violating tridiagonal:")
+        for p in tri:
+            print('({},{})'.format(p[0],p[1]))
+    else:
+        print("Matrix is tridiagonal")
 
 if __name__ == '__main__':
-    setA(4)
-    for i in range(1, 5):
-        setA(i)
-        print(i*A)
+    for i in range(3, 9):
+        doTest(i)
